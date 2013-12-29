b43f64c317d0f9e506cd7fa17184e1c39212beba
Khafaga <dev.khafaga@yahoo.com>
2013-12-29 08:11:44 +0400

    moved all the business logic inside the task , and update the web view after execution.

--- src/com/xeeapps/utils/GetLyricsTask.java
+++ src/com/xeeapps/utils/GetLyricsTask.java
@@ -1,6 +1,8 @@
 package com.xeeapps.utils;
 
 import android.os.AsyncTask;
+import android.webkit.WebView;
+import com.xeeapps.service.SongDetails;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -17,13 +19,28 @@ import java.net.URLEncoder;
  * Time: 08:53 ص
  * To change this template use File | Settings | File Templates.
  */
-public class GetLyricsTask extends AsyncTask<String,Void,String> {
-
-
+public class GetLyricsTask extends AsyncTask<SongDetails,WebView,String> {
+        private WebView lyricsView ;
+    public GetLyricsTask(WebView lyricsView){
+                                       this.lyricsView = lyricsView;
+    }
 
     @Override
-    protected String doInBackground(String... urls) {
+    protected void onPostExecute(String s) {
+        lyricsView.loadDataWithBaseURL(null, s, "text/html", "UTF-8", null);
+    }
+
+    @Override
+    protected String doInBackground(SongDetails... currentSongDetails) {
 //
+        Document doc;
+        String text = null  ;
+        try {
+        String[] songSlices = currentSongDetails[0].getSongData().split("/");
+        String artistName = currentSongDetails[0].getArtistName().trim();
+            artistName =  URLEncoder.encode(artistName,"utf-8");
+        String title = songSlices[songSlices.length - 1].replace(".mp3", "").replace(artistName, "").replaceAll("(.-)*[0-9]*", "").trim();
+       title =  URLEncoder.encode(title, "utf-8");
 //                       String all = "";
 //        URL yahoo = null;
 //        try {
@@ -54,18 +71,27 @@ public class GetLyricsTask extends AsyncTask<String,Void,String> {
 //        } catch (Exception e) {
 //            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
 //        }
-           Document doc;
-        String text = null  ;
-        try {
+
+
             //"http://www.azlyrics.com/lyrics/enriqueiglesias/hero.html"
 
-            Connection connection =   Jsoup.connect("http://hulyrics.com/LyricsService/webresources/service/getLyricBySongCriteria?title="+ URLEncoder.encode(urls[0], "utf-8")+"&artistName="+ URLEncoder.encode(urls[1],"utf-8"));
+            Connection connection =   Jsoup.connect("http://hulyrics.com/LyricsService/webresources/service/getLyricBySongCriteria?title="+ title+"&artistName="+ artistName);
             connection.timeout(60000);
             doc = connection.get();
             Elements lyrics = doc.getElementsByTag("words");
             System.out.println("divs size:"+lyrics.size());;
             Element elem  = lyrics.get(0);
              text =      elem.text();
+
+            if (text == null) text = "";
+            text = "<body style=' -webkit-text-stroke: 1px black;\n" +
+                    "   color: white;\n" +
+                    "   text-shadow:\n" +
+                    "       3px 3px 0 #000,\n" +
+                    "     -1px -1px 0 #000,  \n" +
+                    "      1px -1px 0 #000,\n" +
+                    "      -1px 1px 0 #000,\n" +
+                    "       1px 1px 0 #000;;font-weight:bolder;'>" + text + "</body>";
 
             // lyricsView.setText(elem.text());
             //  System.out.println("elem text:"+elem.text());
