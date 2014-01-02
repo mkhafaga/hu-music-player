155a58bc7b819e9cc64ba91fa02e383f9d9eb5d5
Khafaga <dev.khafaga@yahoo.com>
2014-01-02 23:44:21 +0400

    bug fix: previously when you call music player from background , you get blank album art image.

--- src/com/xeeapps/HuPlayer/SongsActivity.java
+++ src/com/xeeapps/HuPlayer/SongsActivity.java
@@ -1,4 +1,4 @@
-package com.xeeapps.AmPlayer;
+package com.xeeapps.HuPlayer;
 
 import android.app.ListActivity;
 import android.content.Intent;
@@ -35,7 +35,7 @@ public class SongsActivity extends ListActivity {
            // proj = {MediaStore.Audio.Playlists.Members.DISPLAY_NAME,MediaStore.Audio.Playlists.Members.DATA,MediaStore.Audio.Playlists.Members._ID};
 
     proj = { MediaStore.Audio.Media.TITLE,//EDISPLAY_NAME,
-			MediaStore.Audio.Media.DATA, MediaStore.Audio.Media._ID,MediaStore.Audio.Media.ARTIST};
+			MediaStore.Audio.Media.DATA, MediaStore.Audio.Media._ID,MediaStore.Audio.Media.ARTIST,MediaStore.Audio.Media.ALBUM_ID};
 
 	private Uri songsUri;
 
@@ -99,40 +99,73 @@ public class SongsActivity extends ListActivity {
 //           songDetailsList[i].setArtistName(songsCursor.getString(3));
        }
 		songsView = getListView();
+        songsView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int i) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView, int i, int i2, int i3) {
+             Log.i("i - i2 - i3",i+" - "+i2+" - "+i3);
+            }
+        });
 		songsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 			public void onItemClick(AdapterView<?> adapterView, View view,
 					int i, long l) {
                      Mapper song = (Mapper) songsView.getItemAtPosition(i);
 				songsCursor.moveToPosition(song.getId());
-                int childrenCount  =  songsView.getCount();
-                for(int j=0;j<childrenCount;j++){
-                    LinearLayout layout = (LinearLayout) songsView.getChildAt(j);
-                    ImageView imageView  = (ImageView) layout.findViewById(R.id.marked);
-                    if(i==j){
-                        imageView.setImageResource(R.drawable.marked);
-                    }else{
-                        imageView.setImageBitmap(null);
-                    }
+
+//                int childrenCount  =  maker.getRowsValues().size();
+//                for(int j=0;j<childrenCount;j++){
+//                    LinearLayout layout = (LinearLayout) songsView.getChildAt(j);
+//                    ImageView imageView  = (ImageView) layout.findViewById(R.id.marked);
+//                    if(i==j){
+//                        imageView.setImageResource(R.drawable.marked);
+//                        Log.i("mark:",i+"");
+//                    }else{
+//                        imageView.setImageBitmap(null);
+//                    }
 
 
-                }
+//                }
 
 //				Globals.CURRENT_SONG = songsCursor.getString(1);
 //				Globals.SONG_STATE = Globals.RUNNING_SONG;
 //				Globals.CURRENT_CURSOR = songsCursor;
-				Log.i("view class", view.getClass() + "");
+			//	Log.i("view class", view.getClass() + "");
 				Intent playerIntent = new Intent(getApplicationContext(),
 						PlayerActivity.class);
-                playerIntent.setAction("start");
 
-                playerIntent.putExtra("albumArtPath",albumArtPath) ;    // songsCursor.getPosition()
 
-			playerIntent.putExtra("currentSongDetails", new SongDetails(songsCursor.getString(1),songsCursor.getString(0),songsCursor.getString(3)));
-                playerIntent.putExtra("currentSongIndex",songsCursor.getPosition());
-				playerIntent.putExtra("songDetailsList", songDetailsList);
+              //  if(albumArtPath==null){
+                    Cursor albumCursor =  managedQuery(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,new String[]{MediaStore.Audio.Albums.ALBUM_ART,MediaStore.Audio.Albums._ID},"_id=?",new String[]{songsCursor.getString(4)},null) ;
+                    albumCursor.moveToFirst();
+                   albumArtPath =  albumCursor.getString(0) ;
+                 //   albumCursor.close();
+                    // songsCursor.getPosition()
+          //      }
+              SongDetails currentSongDetails = new SongDetails(songsCursor.getString(1),songsCursor.getString(0),songsCursor.getString(3));
+                if(Globals.CURRENT_SONGDETAILS!=null&&currentSongDetails.equals(Globals.CURRENT_SONGDETAILS)){
+                //    Intent playerIntent = new Intent(this,PlayerActivity.class) ;
 
-				startActivity(playerIntent);
+                        playerIntent.setAction("recover");
+//            playerIntent.putExtra("albumArtPath", Globals.ALBUM_ART_PATH) ;    // songsCursor.getPosition()
+//            playerIntent.putExtra("currentSongDetails", Globals.CURRENT_SONG_DETAILS);
+//            playerIntent.putExtra("currentSongIndex",Globals.CURRENT_SONG_INDEX);
+//            playerIntent.putExtra("songDetailsList", Globals.SONG_DETAILS_LIST);
+                        startActivity(playerIntent);
+                } else{
+                    playerIntent.setAction("start");
+                    playerIntent.putExtra("albumArtPath",albumArtPath) ;
+                    playerIntent.putExtra("currentSongDetails", currentSongDetails);
+                    playerIntent.putExtra("currentSongIndex",songsCursor.getPosition());
+                    playerIntent.putExtra("songDetailsList", songDetailsList);
+
+                    startActivity(playerIntent);
+                }
+
 
 			}
 		});
